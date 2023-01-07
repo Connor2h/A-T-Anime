1f2c11b699ced71a49d199390f0ffcd6335eb5df
Jacob Spevak <107512577+captain-stacks@users.noreply.github.com>
2023-01-06 20:32:08 -0600

    hard coded server URL removed vercel.json

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ import YourList from './pages/YourList';
 import Registersignup from './pages/Registerlogin';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'https://a-t-anime-server.vercel.app/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
